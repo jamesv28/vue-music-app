407a58331d6d8e616106b6504fbb151ca56829f2
James Volmert <james.volmert@gmail.com>
2022-01-05 20:24:31 -0700

    added logic for login validation

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -50,31 +50,46 @@
           </ul>
 
           <!-- Login Form -->
-          <form v-show="tab === 'login'">
+          <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="login_show_alert" :class="bg_alert_variant">
+            {{login_alert_msg}}
+          </div>
+          <vee-form 
+            v-show="tab === 'login'"
+            @submit="signin"
+            :validation-schema="loginSchema"
+          >
             <!-- Email -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <input type="email"
+              <label class="inline-block mb-2" for="l_email">Email</label>
+              <vee-field 
+                name="email"
+                id="l_email"
+                type="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email" 
                 autocomplete="off"/>
+              <ErrorMessage class="text-red-600" name="email"/>
             </div>
             <!-- Password -->
             <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input type="password"
+              <label class="inline-block mb-2" for="l_password">Password</label>
+              <vee-field 
+                name="password"
+                id="l_password"
+                type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password" 
                 autocomplete="off"/>
+              <ErrorMessage class="text-red-600" name="password"/>
             </div>
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                 hover:bg-purple-700">
               Submit
             </button>
-          </form>
+          </vee-form>
           <!-- Registration Form -->
           <div class="text-white text-center font-bold p-4 rounded mb-4" v-if="reg_show_alert" :class="bg_alert_variant">
             {{reg_alert_msg}}
@@ -196,6 +211,9 @@ export default {
         userData: {
           country: 'USA'
         },
+        login_in_submission: false,
+        login_show_alert: false,
+        login_alert_msg: 'Please wait! Your login is being processed',
         reg_in_submission: false,
         reg_show_alert: false,
         bg_alert_variant: 'bg-blue-500',
@@ -209,6 +227,10 @@ export default {
           confirm_password: 'required|confirmed:@password',
           country: 'required',
           tos: 'tos'
+        },
+        loginSchema: {
+          email: 'required|min:3|max:100|email',
+          password: 'required|min:3|max:100'
         }
       }
     },
@@ -227,6 +249,17 @@ export default {
 
         this.bg_alert_variant = 'bg-green-500';
         this.reg_alert_msg = "Congrats! Your account has been created";
+
+        console.log('values', values);
+      },
+      signin(values) {
+        this.login_show_alert = true;
+        this.login_in_submission = true;
+        this.bg_alert_variant = 'bg-blue-500';
+        this.login_alert_msg = "Please wait. your account is being created";
+
+        this.bg_alert_variant = 'bg-green-500';
+        this.login_alert_msg = "Congrats! Your account has been created";
 
         console.log('values', values);
       }
