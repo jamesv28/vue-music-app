64b7ca78dc3b65be765724b020b37a09c73ece6e
James Volmert <james.volmert@gmail.com>
2022-01-17 19:47:17 -0700

    started working on managing my collection components

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -2,53 +2,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <div
-          class="bg-white rounded border border-gray-200 relative flex flex-col"
-        >
-          <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">Upload</span>
-            <i class="fas fa-upload float-right text-green-400 text-2xl"></i>
-          </div>
-          <div class="p-6">
-            <!-- Upload Dropbox -->
-            <div
-              class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
-            >
-              <h5>Drop your files here</h5>
-            </div>
-            <hr class="my-6" />
-            <!-- Progess Bars -->
-            <div class="mb-4">
-              <!-- File Name -->
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <!-- Inner Progress Bar -->
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 75%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 35%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="font-bold text-sm">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                <div
-                  class="transition-all progress-bar bg-blue-400"
-                  style="width: 55%"
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <upload />
       </div>
       <div class="col-span-2">
         <div
@@ -57,7 +11,7 @@
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
             <span class="card-title">My Songs</span>
             <i
-              class="fa fa-compact-disc float-right text-green-400 text-2xl"
+              class="fa fa-compact-disc float-right text-emerald-400 text-2xl"
             ></i>
           </div>
           <div class="p-6">
@@ -66,12 +20,12 @@
               <div>
                 <h4 class="inline-block text-2xl font-bold">Song Name</h4>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-rose-500 float-right"
                 >
                   <i class="fa fa-times"></i>
                 </button>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-indigo-500 float-right"
                 >
                   <i class="fa fa-pencil-alt"></i>
                 </button>
@@ -96,7 +50,7 @@
                   </div>
                   <button
                     type="submit"
-                    class="py-1.5 px-3 rounded text-white bg-green-600"
+                    class="py-1.5 px-3 rounded text-white bg-emerald-600"
                   >
                     Submit
                   </button>
@@ -113,12 +67,27 @@
               <div>
                 <h4 class="inline-block text-2xl font-bold">Song Name</h4>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-rose-500 float-right"
                 >
                   <i class="fa fa-times"></i>
                 </button>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-indigo-500 float-right"
+                >
+                  <i class="fa fa-pencil-alt" aria-label="Edit"></i>
+                </button>
+              </div>
+            </div>
+            <div class="border border-gray-200 p-3 mb-4 rounded">
+              <div>
+                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
+                <button
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-rose-500 float-right"
+                >
+                  <i class="fa fa-times"></i>
+                </button>
+                <button
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-indigo-500 float-right"
                 >
                   <i class="fa fa-pencil-alt"></i>
                 </button>
@@ -128,12 +97,12 @@
               <div>
                 <h4 class="inline-block text-2xl font-bold">Song Name</h4>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-rose-500 float-right"
                 >
                   <i class="fa fa-times"></i>
                 </button>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-indigo-500 float-right"
                 >
                   <i class="fa fa-pencil-alt"></i>
                 </button>
@@ -143,27 +112,12 @@
               <div>
                 <h4 class="inline-block text-2xl font-bold">Song Name</h4>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-rose-500 float-right"
                 >
                   <i class="fa fa-times"></i>
                 </button>
                 <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
-                >
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
-                >
-                  <i class="fa fa-times"></i>
-                </button>
-                <button
-                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+                  class="ml-1 py-1 px-2 text-sm rounded text-white bg-indigo-500 float-right"
                 >
                   <i class="fa fa-pencil-alt"></i>
                 </button>
@@ -178,6 +132,7 @@
 
 <script>
 // import store from '@/store';
+import Upload from '@/components/Upload.vue';
 
 export default {
   name: 'ManageView',
@@ -191,5 +146,8 @@ export default {
   //     })
   //   }
   // }
+  components: {
+    Upload
+  }
 };
 </script>
